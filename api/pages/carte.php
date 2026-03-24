20ec11ff61bfb48d3f811ab827c843a67e9703f8
The-Night7 <myriam.bensaid21@gmail.com>
2026-03-24 08:45:36 +0100

    fix(api): correct header.php include path in carte.php

--- api/pages/carte.php
+++ api/pages/carte.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/includes/header.php';
+require_once __DIR__ . '/../includes/header.php';
 require_once __DIR__ . '/../includes/config.php';
 require_once __DIR__ . '/../includes/plats.php';
 require_once __DIR__ . '/../includes/panier.php';
