b1f0aa3dcd4e4a33919388a98ef4336cf1f933cd
The-Night7 <myriam.bensaid21@gmail.com>
2026-03-25 12:19:06 +0100

    fix: remove persistent database connections from PDO options

--- api/includes/config.php
+++ api/includes/config.php
@@ -35,7 +35,6 @@ try {
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_PERSISTENT         => true,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
 
